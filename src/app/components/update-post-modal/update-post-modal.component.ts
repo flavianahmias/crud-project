db607cda426191acef9dda49138040a114b5c93b
flaviaanahmias <flaviaanahmias@gmail.com>
2025-02-19 22:08:47 -0400

    feat: create UpdatePostModal

---
+++ src/app/components/update-post-modal/update-post-modal.component.ts
@@ -0,0 +1,52 @@
+import {
+  Component,
+  ChangeDetectionStrategy,
+  inject,
+  model,
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogModule,
+  MatDialogRef,
+  MatDialogTitle,
+} from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
+export interface InputData {
+  title: string;
+  body: string;
+}
+@Component({
+  selector: 'update-post-modal',
+  templateUrl: 'update-post-modal.html',
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatButtonModule,
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatDialogClose,
+  ],
+})
+export class UpdatePostModal {
+  readonly dialogRef = inject(MatDialogRef<UpdatePostModal>);
+  readonly data = inject<InputData>(MAT_DIALOG_DATA);
+  readonly title = model(this.data.title);
+  readonly body = model(this.data.body);
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+  onClose() {
+    return { ...this.data, title: this.title(), body: this.body() };
+  }
+}
